dfd0e4c38621a9c6de1202b46f7b06e3d12158b4
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2021-09-22 09:50:14 +0000

    bal en platform gemaakt, platform beweegt

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function setup() {
-	createCanvas(500, 400);
+class Ball{
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+  }
+
+  draw() {
+    ellipse(this.x, this.y, this.width, this.height);
+  }
 }
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
+var ball1, [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
+function setup() {
+	createCanvas(500, 400);
+
+    ball1 = new Ball(225,225,50,50);
+}
+
+
 
 function draw() {
 	background(225);
@@ -11,7 +29,8 @@ function draw() {
 	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
 	
 	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
+	rect(xpos-75, ypos+125, 200, 10);
+  ball1.draw();
 	
 	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
 	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
