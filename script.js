b7ca8e7c165b6fd29ef6668280591bdc17efe7b5
Bart van <57255021+Bart1003@users.noreply.github.com>
2021-10-04 12:05:42 +0000

    Rework van de class Rect

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Ball{
+class Ball {
   constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
@@ -11,66 +11,69 @@ class Ball{
   }
 }
 
-class Rect{
-  constructor(x, y, w, h){
+class Rect {
+  constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.width = w;
     this.height = h;
   }
 
+  draw() {
+    rect(this.x, this.y, this.width, this.height);
+  }
 }
-var ball1, [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
+var ball1, paddle, [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
 
 function setup() {
-	createCanvas(500, 400);
+  createCanvas(500, 400);
 
-    ball1 = new Ball(225,225,50,50);
+  ball1 = new Ball(225, 225, 50, 50);
+  paddle = new Rect(150, 350, 200, 10);
 }
 
 
 
 function draw() {
-	background(255);
-	
-	fill(0);
-	text("Use the arrow keys, left and right (or AD) to move the square around", 25, 25);
+  background(125);
 
-	fill(0, 0, 0);
-  rectMode(CORNER);
-	rect(xpos-75, ypos+125, 200, 10);
+  fill(0);
+  text("Use the arrow keys, left and right (or AD) to move the square around", 25, 25);
+
+  fill(0, 0, 0);
   ball1.draw();
-	
-	
-  if( xpos  >= 400 || xpos <= 0 ) {
+  paddle.draw();
+
+  if (xpos >= 400 || xpos <= 0) {
     xpos = 400;
   }
-  else 
+  else
     xpos += -xspeed;
 }
 
 function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-	}
+  switch (keyCode) {
+    case 37:
+    case 65:
+      
+      break;
+    case 39:
+    case 68:
+      
+      break;
+  }
 }
 
 function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-	}
+  switch (keyCode) {
+    case 37:
+    case 65:
+      
+      break;
+    case 39:
+    case 68:
+      
+      break;
+  }
 }
